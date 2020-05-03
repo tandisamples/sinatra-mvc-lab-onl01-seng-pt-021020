38eead0ac3e80449b1c0887eb5a53171e1b98a8d
Tandi Samples <tandisamples@gmail.com>
2020-05-03 19:22:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+        erb :user_input
+  end
+
+    post '/piglatinize' do 
+        pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :done
+    end
 end
